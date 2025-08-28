40fac8e2f6b3d9aeb5d061522253cd3367328aaf
Ben <150054499+Beleebs@users.noreply.github.com>
2025-08-28 15:44:37 -0400

    Added more things into the cli11 learning grounds.

--- cli11 shenanigans/attributor.cpp
+++ cli11 shenanigans/attributor.cpp
@@ -5,19 +5,22 @@
 #include <iostream>
 #include "CLI11.hpp"
 
+// I think I want the input to go along the lines of:
+// ./attributor -i [JSON file] -o [XML file]
+
 int main(int argc, char **argv) {
     CLI::App app{"srcAttributor is a property generator based on srcSlice outputs.", "srcAttributor"};
 
     int p = 0;
-    std::string file;
-    app.add_option("-p", p, "Parameter");
-    app.add_option("-i,--input", file, "File Input (Requires existing file)")
+    std::string fileIn, fileOut;
+    app.add_option("-i,--input", fileIn, "JSON File Input")
+        ->required()
+        ->check(CLI::ExistingFile);
+    app.add_option("-o,--output", fileOut, "XML File Output")
         ->required()
         ->check(CLI::ExistingFile);
 
     CLI11_PARSE(app, argc, argv);
 
-    std::cout << "Parameter value: " << p << std::endl;
-    std::cout << "Filepath provided: " << file << std::endl;
     return 0;
 }
