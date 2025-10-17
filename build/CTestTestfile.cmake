cdb151a533f06a1fa86c951b5f12e31f959b7dbf
Ben Leber <150054499+Beleebs@users.noreply.github.com>
2025-10-17 04:05:24 -0400

    Delete build directory
    hiding this

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/super/Desktop/GitHub/srcAttributor
-# Build directory: /mnt/c/Users/super/Desktop/GitHub/srcAttributor/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
